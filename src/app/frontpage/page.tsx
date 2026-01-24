3c87963c03ebdd3156edc9436128025bdc04882c
CoocooFroggy <45371102+CoocooFroggy@users.noreply.github.com>
2026-01-24 15:49:30 -0600

    Frontpage for Folashewa
    Co-authored-by: ayashkarabek <ayashkarabek@users.noreply.github.com>
    Co-authored-by: jnwang12 <jnwang12@users.noreply.github.com>
    Co-authored-by: Michelle <Folashewa@users.noreply.github.com>
    Co-authored-by: Serena43 <Serena43@users.noreply.github.com>

---
+++ src/app/frontpage/page.tsx
@@ -0,0 +1,19 @@
+// blue bubble component for user response
+
+const styles = {
+    bubble: {
+        height: "300px",
+        width: "300px",
+        border: "solid 1px blue" ,
+    },
+}
+
+// 
+export default function MessageBubble() {
+  // Add message bubble exponent on screen
+  return (
+    <div>
+      Hello
+    </div>
+  );
+}
