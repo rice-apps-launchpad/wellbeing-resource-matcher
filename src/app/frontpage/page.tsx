a03599fb3a19c1eea70621c08103d67088f88ec0
Michelle <olaniyifolashewa@gmail.com>
2026-01-31 14:27:46 -0600

    I added a message bubble

--- src/app/frontpage/page.tsx
+++ src/app/frontpage/page.tsx
@@ -1,19 +1,37 @@
-// blue bubble component for user response
-
 const styles = {
+  // bubble
     bubble: {
-        height: "300px",
-        width: "300px",
-        border: "solid 1px blue" ,
+      maxWidth: "200px",
+      display: "inline-block",
+      borderRadius: "10px",
+      padding: "4px",
+      overflowWrap: "break-word",
     },
-}
 
-// 
+    userBubble: {
+      color: "white",
+      backgroundColor: "#3E5C93",
+      border: "1px solid #3E5C93",
+    },
+
+    aiBubble: {
+      color: "black",
+      backgroundColor: "#FFFFFF",
+      border: "1px solid #BEBFBF",
+    },
+} as const;
+
 export default function MessageBubble() {
-  // Add message bubble exponent on screen
   return (
     <div>
-      Hello
+    <div style={{...styles.bubble, ...styles.userBubble}}>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt?
+    </div>
+    <div style={{...styles.bubble, ...styles.aiBubble}}>
+      Sed ut perspiciatis unde omnis iste natus error sit voluptatem: 
+      Office of Academic Advising. 
+      udantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi.
+    </div>
     </div>
   );
 }
