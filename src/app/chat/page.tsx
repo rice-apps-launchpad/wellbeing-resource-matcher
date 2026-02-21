278f9d17513a5e91b78143d0e60b5917a8689ee4
CoocooFroggy <45371102+CoocooFroggy@users.noreply.github.com>
2026-02-21 13:45:25 -0600

    Chat updates

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -4,7 +4,7 @@ This will be the "chat" page for both desktop and mobile! It can take infinitely
 automatically.
  */
 
-import {useRef, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import {ChatMessage, Sender} from "@/data/chat-message";
 import MessageBubble from "@/components/message-bubble";
 
@@ -12,15 +12,30 @@ export default function ChatPage() {
   const chatInputRef = useRef<HTMLInputElement>(null);
   const [messages, setMessages] = useState<ChatMessage[]>([]);
 
+  // When `messages` changes, we might need to scroll to bottom
+  useEffect(() => {
+    // https://stackoverflow.com/a/21067431
+    const scrollView = scrollViewRef.current;
+    if (!scrollView) return;
+    console.log(scrollView.scrollHeight)
+    console.log(scrollView.clientHeight)
+    console.log(scrollView.scrollTop + 1)
+    const isScrolledToBottom = scrollView.scrollHeight - scrollView.clientHeight <= scrollView.scrollTop + 1;
+    if (isScrolledToBottom)
+      scrollView.scrollTop = scrollView.scrollHeight - scrollView.clientHeight;
+  }, [messages]);
+
+  const scrollViewRef = useRef<HTMLDivElement>(null);
+
   return (
-    <div className={"w-100 h-screen flex flex-col justify-end bg-[#E8E8E8] p-5"}>
+    <div className={"w-100 h-screen flex flex-col justify-end bg-[#E8E8E8] pb-5 pl-5"}>
       {/* This div holds all the messages */}
-      <div className={"flex flex-col items-end gap-3"}>
+      <div ref={scrollViewRef} className={"flex flex-col items-end gap-3 overflow-scroll pr-5"}>
         {messages.map((message, index) => {
           return (<MessageBubble message={message} key={index}/>)
         })}
       </div>
-      <input className={"mt-5 mb-5 bg-white rounded-2xl border-[0.5px] border-[#9BA9B0] p-1"}
+      <input className={"mt-5 mb-5 bg-white rounded-2xl border-[0.5px] border-[#9BA9B0] p-1 mr-5"}
              placeholder={"Type your message..."}
              ref={chatInputRef}
              onKeyDown={event => {
